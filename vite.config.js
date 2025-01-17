99659ae3aa49ffbe62311aefcdd9c01901046ef2
muhammetsefa2 <162960315+muhammetsefa1@users.noreply.github.com>
2025-01-17 12:52:51 -0500

    Yapmaya çaışıyorum

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-new-app/', // Burayı GitHub repo adınızla değiştirin
 });
